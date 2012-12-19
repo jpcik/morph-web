e2b4654d2ffc3d25cd4e926f36fcd11ad15c903c
jpcik <jean.paul.ik@gmail.com>
2012-12-19 20:25:49 +0100

    Added Emt query removed config files updated to 1.0.1 morph-streams

--- sparqlstream-web/project/Build.scala
+++ sparqlstream-web/project/Build.scala
@@ -5,11 +5,11 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
     val appName         = "sparqlstream-web"
-    val appVersion      = "1.0-SNAPSHOT"
+    val appVersion      = "1.0.1"
 
     val appDependencies = Seq(
       // Add your project dependencies here,
-      "es.upm.fi.oeg.morph.streams" % "adapter-gsn" % "1.0.0"
+      "es.upm.fi.oeg.morph.streams" % "adapter-gsn" % "1.0.1"
 
     )
 
@@ -20,8 +20,8 @@ object ApplicationBuild extends Build {
          //("Local ivy Repository" at "file://"+Path.userHome.absolutePath+"/.ivy2/local")
          Resolver.url("local ivy",url("file://"+Path.userHome.absolutePath+"/.ivy2/local"))(Resolver.ivyStylePatterns),
          "aldebaran-libs" at "http://aldebaran.dia.fi.upm.es/artifactory/sstreams-external-libs-local",
-         "aldebaran-releases" at "http://aldebaran.dia.fi.upm.es/artifactory/sstreams-releases-local",
-         "jpc-repo" at "https://github.com/jpcik/jpc-repo/raw/master/repo"   
+         "aldebaran-releases" at "http://aldebaran.dia.fi.upm.es/artifactory/sstreams-releases-local"
+         //"jpc-repo" at "https://github.com/jpcik/jpc-repo/raw/master/repo"   
         )
     )
 
